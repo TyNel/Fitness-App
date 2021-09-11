8165c6010b328d4f442f01e1e043ea2c4375eaf2
tyler nelson <TyNel94@outlook.com>
2021-09-10 20:29:47 -0700

    Deleted some unused files, oganized files into Dashboard and UserLogin. Got UI set up for dashboard page and login page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dashboard from "./Dashboard/Dashboard";
+import { withRouter, BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./UserLogin/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/Dashboard" component={Dashboard} />
+        <Route path="/Login" component={Login} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default withRouter(App);
